9262c299e37806ad825aceea8c17fe227c61cd72
SAMIT KUMAR BARIK <126311037+samit1998@users.noreply.github.com>
2023-07-06 14:31:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 //your JS code here. If required.
-let select = document.getElementById("colorSelect");
-let btn =  document.querySelector("input[type=button]")
-btn.addEventListener("click", ()=>{       
-	//select.children--> will give all the child elements of select element
-    for (const child of select.children) {
-		//select.value--> will give value of the selected element
-        if(child.value === select.value){
-			//child.remove()-->remove the child element
-                child.remove();
-         }
-    }
-})
+llet height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+    h.innerText="Width: "+width+" and Height: "+height;  
+window.addEventListener("resize",function(){
+    let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+ 
+    h.innerText="Width: "+width+" and Height: "+height; 
+  
+  })
